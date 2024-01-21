50a45d85291ec22c5c71376eedd397582e9a92b1
manjunath k <manjunath.kiudanavar@gmail.com>
2024-01-21 23:51:45 +0530

    second step of the code java

--- manju.java
+++ manju.java
@@ -1,5 +1,13 @@
 class FirstProgram{
     public static void main(String[] args){
-        System.out.println("hello freinds");
-    }
+        System.out.print("some of these numbers is :");
+
+    int num1 = 6;
+    int num2 = 8;
+    int num3 = 7;
+    int sum = num1 + num2 + num3;
+    
+    System.out.println(sum) ;
 }
+    }
+ 
