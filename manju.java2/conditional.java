fbed6cf47496da79561b2b3833125b9b26a6ed77
manjunath k <manjunath.kiudanavar@gmail.com>
2024-02-19 21:37:18 +0530

    if else statements

---
+++ manju.java2/conditional.java
@@ -0,0 +1,14 @@
+public class conditional {
+    public static void main(String[] args) {
+        boolean rainy = true;
+        boolean carryumbrella;
+        if(rainy == true){
+            carryumbrella = true;
+        }
+        else{
+            carryumbrella = false;
+        }
+        System.out.println(carryumbrella);
+    }
+    
+}
